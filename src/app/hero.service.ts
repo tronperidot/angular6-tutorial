7c0b1fe08d530b1ac5ce534afbf01c81e0ab96a7
tronperidot <tronperidot@gmail.com>
2018-10-13 19:27:36 +0900

    tutorial add router.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,8 +8,12 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
-
   constructor(private messageService: MessageService) { }
+
+  getHero(id: number): any {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
